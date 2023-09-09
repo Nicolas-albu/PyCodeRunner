62a9c7419d5891571a1000af6750531088a9d848
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-09-09 05:48:45 -0300

    Adicionado customização da mensagem de erro de validação do Pydantic

--- PyCodeRunner/main.py
+++ PyCodeRunner/main.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 
 from .routers import router
 
 app = FastAPI()
 
 app.include_router(router)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(
+    request: Request,
+    exc: RequestValidationError,
+):
+    for error in exc.errors():
+        error.pop("ctx")
+        error.pop("loc")
+        error.pop("input")
+        error.pop("url")
+        error["message"] = error.pop("msg")
+
+    return JSONResponse(
+        content=jsonable_encoder({"detail": exc.errors()}),
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+    )
